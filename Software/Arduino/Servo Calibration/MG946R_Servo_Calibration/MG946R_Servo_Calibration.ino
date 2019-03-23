0ddaca5489736550c614c4c84a2435930e3e249d
Jihad Alsamarji <jihadsamarji@gmail.com>
2019-03-23 13:04:16 +0100

    Fix data.txt file

--- Software/Arduino/Servo Calibration/MG946R_Servo_Calibration/MG946R_Servo_Calibration.ino
+++ Software/Arduino/Servo Calibration/MG946R_Servo_Calibration/MG946R_Servo_Calibration.ino
@@ -11,7 +11,7 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
+int pos = 9;    // variable to store the servo position
 
 void setup() {
   Serial.begin(9600);
